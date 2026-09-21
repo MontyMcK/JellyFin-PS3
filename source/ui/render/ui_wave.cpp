e9c49609425224813fc1ee951da5d45ad6fb7442
vortigauntlet <rocketleagueracist@gmail.com>
2026-09-21 23:01:58 +0100

    fix: close JellyWave test branch

--- source/ui/render/ui_wave.cpp
+++ source/ui/render/ui_wave.cpp
@@ -1086,7 +1086,8 @@ void wave_draw(void) {
                 rsxInvalidateVertexCache(context);
                 rsxDrawVertexArray(context, GCM_TYPE_TRIANGLE_STRIP, s_jw_off[0][0] + section_v, section_v);
             }
-        } else {{
+        }
+    } else {{
             const u32 stripv  = (u32)(ncols * 2);
             const int nstrips = s_wave_blend ? 3 : (3 * WAVE_NS);
             for (int s = 0; s < nstrips; s++) {
